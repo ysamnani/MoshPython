76a24f00bad01d32d17e94ff48722f23ad88d3b7
ysamnani <yash.samnani@gmail.com>
2019-03-21 15:19:31 +0530

    updated program

--- Test1.py
+++ Test1.py
@@ -1,5 +1,15 @@
 # 21-Mar-2019
-for i in range(2):
-    for j in range(5,0,-3):
-        print ("X" * j)
-print ("X" * (i+1))
+list = [1,5,9,3,7,3, 41, -2, 900]
+# find largest number using rudimentary algo
+
+y = range(len(list))
+print (y)
+max_value = 0
+for i in y:
+    if list[i] > max_value:
+        max_value = list[i]
+    #else:
+    #    max_value = list[i+1]
+    print (i, "|", list[i], "|", max_value)
+
+print (f"Max value in give list = {max_value}")
