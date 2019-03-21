ea2a9f367a1748792be627bf88a572c1ab2db57b
ysamnani <yash.samnani@gmail.com>
2019-03-21 16:04:03 +0530

    updated program

--- Test1.py
+++ Test1.py
@@ -1,15 +1,10 @@
 # 21-Mar-2019
-list = [1,5,9,3,7,3, 41, -2, 900]
+list = [9,3,7,41,-2,90]
 # find largest number using rudimentary algo
-
 y = range(len(list))
-print (y)
 max_value = 0
-for i in y:
-    if list[i] > max_value:
-        max_value = list[i]
-    #else:
-    #    max_value = list[i+1]
-    print (i, "|", list[i], "|", max_value)
-
+for i in list:
+    if i > max_value:
+        max_value = i
+    print (i, "|", i, "|", max_value)
 print (f"Max value in give list = {max_value}")
